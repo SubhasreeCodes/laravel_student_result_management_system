40c72b54cfc62286594b210382e66004272a6b5d
SubhasreeCodes <subhasreeps2001@gmail.com>
2024-12-26 19:48:59 +0530

    GroupSeeder added to DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,5 +19,8 @@ class DatabaseSeeder extends Seeder
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+
+        // call the Group Seeder class
+        $this->call(GroupSeeder::class);
     }
 }
