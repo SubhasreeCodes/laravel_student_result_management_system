525ea4c471d29b5d1a538852afe66fc2402a5c31
SubhasreeCodes <subhasreeps2001@gmail.com>
2024-12-27 18:07:36 +0530

    Create StudentGroupSection.php
    StudentGroupSection  belongto Student  relationship added

---
+++ app/Models/StudentGroupSection.php
@@ -0,0 +1,14 @@
+<?php
+namespace App\Models;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class StudentGroupSection extends Model
+{
+    use HasFactory;
+    // Define the relationship with the Student model
+    public function student()
+    {
+        return $this->belongsTo(Student::class);
+    }
+}
